6d87f17c36ae3fad99793e89c00a2d2ce58c33ad
Michael McCracken <michael.mccracken172@gmail.com>
2018-03-13 16:58:29 -0400

    circleci, new tests, ready for v1

--- index.js
+++ index.js
@@ -1,8 +1,53 @@
+var _ = require('lodash')
+var notamKeywords = ['RWY', 'TWY', 'APRON', 'AD', 'OBST', 'NAV', 'COM', 'SVC', 'AIRSPACE', 'ODP', 'SID', 'STAR', 'CHART', 'DATA', 'IAP', 'VFP', 'ROUTE', 'SPECIAL', 'SECURITY']
+
+var tests = [
+
+  function (str) {
+    return str.startsWith("!") ? true : false
+  },
+
+  function (str) {
+    return str.length < 21 ? false : true
+  },
+
+  function (str) {
+    return str.charAt(4) === " " ? true : false
+  },
+
+  function (str) {
+    var result = false
+    notamKeywords.forEach(function(kw){
+      if ( str.includes(" " + kw + " ") ) {
+        result = true
+      }
+    })
+    return result
+  },
+
+]
+
 function isNotam(str){
-  if ( str.startsWith("!") ) {
-    return true
+  var testResults = _.map(tests,function (test) {
+    return test(str)
+  })
+
+  if ( testResults.includes(false) ) {
+    return false
   }
-  return false
+  return true
 }
 
 module.exports = isNotam
+
+
+/** Criteria
+*
+* begins with !
+* accountability
+* location identifier
+* keyword
+*
+*
+
+*/
--- tests/index.js
+++ tests/index.js
@@ -14,16 +14,43 @@ describe("#isNotam()",function(){
     expect(isNotam).to.be.a("function")
   })
 
-  context("valid NOTAMs",function(){
-    it("validates set",function(){
-      expect(isNotam("!RDU")).to.be.true
-    })
+  context("validates valid NOTAM",function(){
+    assertNotam('!ATL 03/080 ATL RWY 10/28 CLSD 1803120330-1803120700',true)
+    assertNotam('!MYR 12/046 (KMYR A0660/17) MYR TWY A2, A3 CLSD 1712172055-1804012200',true)
+
   })
 
-  context("invalid NOTAMs",function(){
+  context("invalidates NOTAMs with an",function(){
+
     it("empty string", function(){
       expect(isNotam("")).to.be.false
     })
+
+    it("incomplete string",function(){
+      expect(isNotam("!RDU")).to.be.false
+    })
+
+    it("fifth char not a space",function(){
+      expect(isNotam("!LHZDERPARRYANSUPIDSDFAOSDIJFA")).to.be.false
+    })
+
+  })
+
+  it("invalidates without a required keyword", function(){
+    expect(isNotam("!DAB 03/012 DAB ILS 07L GP OUT OF SERVICE 1803052234-1803162000EST")).to.be.false
   })
 
 })
+
+function assertNotam(notam,valid){
+  it("for "+notam,function(){
+    if ( valid ) {
+      expect(isNotam(notam)).to.be.true
+    }
+    else {
+      expect(isNotam(notam)).to.be.false
+    }
+  })
+}
+
+//
